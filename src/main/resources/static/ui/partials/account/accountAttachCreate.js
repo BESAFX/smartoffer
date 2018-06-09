d47500d6b441a1b3cf387034590f7cdb68326edd
Development <islamhaker@gmail.com>
2018-06-09 23:24:08 +0300

    version 0.4

--- src/main/resources/static/ui/partials/account/accountAttachCreate.js
+++ src/main/resources/static/ui/partials/account/accountAttachCreate.js
@@ -109,29 +109,9 @@ app.controller('accountAttachCreateCtrl', [
             document.getElementById('uploader').click();
         };
 
-        $scope.scanFiles = function() {
-            scanner.scan(function (successful, mesg, response) {
-                    var scannedImages = scanner.getScannedImages(response, true, false); // returns an array of ScannedImage
-                    for(var i = 0; (scannedImages instanceof Array) && i < scannedImages.length; i++) {
-                        var scannedImage = scannedImages[i];
-                        // convert base64/URLEncoded data component to raw binary data held in a string
-                        var byteString;
-                        if (scannedImage.src.split(',')[0].indexOf('base64') >= 0)
-                            byteString = atob(scannedImage.src.split(',')[1]);
-                        else
-                            byteString = unescape(scannedImage.src.split(',')[1]);
-                        // separate out the mime component
-                        var mimeString = scannedImage.src.split(',')[0].split(':')[1].split(';')[0];
-                        // write the bytes of the string to a typed array
-                        var ia = new Uint8Array(byteString.length);
-                        for (var i = 0; i < byteString.length; i++) {
-                            ia[i] = byteString.charCodeAt(i);
-                        }
-                        var blob = new Blob([ia], {type:mimeString});
-                        var file = new File([blob], Math.floor((Math.random() * 50000) + 1) + '.jpg');
-                        $scope.files.push(file);
-                    }
-                },
+        //////////////////////////Scan Manager///////////////////////////////////
+        $scope.scanToJpg = function() {
+            scanner.scan(displayImagesOnPage,
                 {
                     "output_settings" :
                         [
@@ -143,6 +123,37 @@ app.controller('accountAttachCreateCtrl', [
                 }
             );
         };
+
+        function displayImagesOnPage(successful, mesg, response) {
+            var scannedImages = scanner.getScannedImages(response, true, false); // returns an array of ScannedImage
+            for(var i = 0; (scannedImages instanceof Array) && i < scannedImages.length; i++) {
+                var scannedImage = scannedImages[i];
+                var blob = dataURItoBlob(scannedImage.src);
+                var file = new File([blob], Math.floor((Math.random() * 50000) + 1) + '.jpg');
+                $scope.files.push(file);
+            }
+        }
+
+        function dataURItoBlob(dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = unescape(dataURI.split(',')[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {type:mimeString});
+        }
+        //////////////////////////Scan Manager///////////////////////////////////
         
         $scope.$watch('files', function (newVal, oldVal) {
             if ($scope.files.length > 0) {
